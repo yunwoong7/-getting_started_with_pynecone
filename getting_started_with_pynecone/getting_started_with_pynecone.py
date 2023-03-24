95957a078f92419fa74988b912d3a62b35e96b9d
yunwoong <yunwoong7@gmail.com>
2023-03-24 13:48:43 +0900

    First Commit

--- getting_started_with_pynecone/getting_started_with_pynecone.py
+++ getting_started_with_pynecone/getting_started_with_pynecone.py
@@ -3,23 +3,22 @@ from pcconfig import config
 
 import pynecone as pc
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
+docs_url = "https://yunwoong.tistory.com/"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(pc.State):
     """The app state."""
-
     pass
 
 
 def index() -> pc.Component:
     return pc.center(
         pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
+            pc.heading("Welcome to Home!", font_size="2em"),
+            pc.box("I am very excited and looking forward to using pynecone."),
             pc.link(
-                "Check out our docs!",
+                "Go to my blog!",
                 href=docs_url,
                 border="0.1em solid",
                 padding="0.5em",
